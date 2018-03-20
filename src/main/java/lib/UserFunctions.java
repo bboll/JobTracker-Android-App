203123a5b2977d890c3eeed4fbaee662ada1aabf
Brian Boll <bboll@kent.edu>
2018-03-20 15:45:01 -0400

    Update UserFunctions.java

--- src/main/java/lib/UserFunctions.java
+++ src/main/java/lib/UserFunctions.java
@@ -15,76 +15,76 @@ public class UserFunctions {
 
     public JSONObject loginUser(String email, String password) {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
     
     public JSONObject registerUser(String name, String email, String password) {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public String getUserName(Context context) {
         //Stub
-        return new String str;
+        return new String;
     }
 
     public String getAdminFlag(Context context) {
         //Stub
-        return new String str;
+        return new String;
     }
 
     public JSONObject createJob()
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public String getUserID(Context context)
     {
         //Stub
-        return new String str;
+        return new String;
     }
 
     public JSONObject getAssignedJobs(String employeeID)
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public JSONObject getUnassignedJobs()
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public JSONObject createJob(String jobName, String contactName, String contactPhone, String address, String scope, String date)
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public JSONObject deleteJob(String jobID)
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public JSONObject assignJob(String jobID, String employeeID)
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public JSONObject getUnassignedEmployees(String jobID)
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public JSONObject toggleClocked(String jobID, String employeeID, String toggleBit)
     {
         //Stub
-        return new JSONObject jObj;
+        return new JSONObject;
     }
 
     public boolean isUserLoggedIn(Context context){
