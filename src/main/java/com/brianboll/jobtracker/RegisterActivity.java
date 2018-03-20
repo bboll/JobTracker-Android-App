6b9ab4b26571bc80ff0ea5f0069b76413f83f664
Brian Boll <bboll@kent.edu>
2018-03-20 15:28:32 -0400

    Create RegisterActivity.java

---
+++ src/main/java/com/brianboll/jobtracker/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.brianboll.jobtracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends Activity {
+
+    //Stub
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
