1bde147d62b2e3b57680d1fe89b4f8bcb438b904
Brian Boll <bboll@kent.edu>
2018-03-20 15:25:48 -0400

    Update UserFunctions.java

--- src/main/java/lib/UserFunctions.java
+++ src/main/java/lib/UserFunctions.java
@@ -15,23 +15,28 @@ public class UserFunctions {
 
     public JSONObject loginUser(String email, String password) {
         //Stub
+        return JSONObect jObj;
     }
     
     public JSONObject registerUser(String name, String email, String password) {
         //Stub
+        return JSONObect jObj;
     }
 
     public String getUserName(Context context) {
         //Stub
+        return String str;
     }
 
     public String getAdminFlag(Context context) {
         //Stub
+        return String str;
     }
 
     public JSONObject createJob()
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public String getUserID(Context context)
@@ -42,44 +47,53 @@ public class UserFunctions {
     public JSONObject getAssignedJobs(String employeeID)
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public JSONObject getUnassignedJobs()
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public JSONObject createJob(String jobName, String contactName, String contactPhone, String address, String scope, String date)
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public JSONObject deleteJob(String jobID)
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public JSONObject assignJob(String jobID, String employeeID)
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public JSONObject getUnassignedEmployees(String jobID)
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public JSONObject toggleClocked(String jobID, String employeeID, String toggleBit)
     {
         //Stub
+        return JSONObect jObj;
     }
 
     public boolean isUserLoggedIn(Context context){
         //Stub
+        return true;
     }
 
     public boolean logoutUser(Context context){
         //Stub
+        return true;
     }
 
 }
